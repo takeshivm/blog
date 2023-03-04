f4b35a8b17d870fe305387de0beafffcae7cbb87
takeshivm <vargasmurillotakeshi@gmail.com>
2023-03-03 19:19:46 -0500

    First commit

---
+++ app/Policies/ChirpPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Chirp;
+use App\Models\User;
+
+class ChirpPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function update(User $user, Chirp $chirp): bool
+    {
+        return $chirp->user()->is($user);
+    }
+
+    public function delete(User $user, Chirp $chirp): bool
+    {
+        return $this->update($user, $chirp);
+    }
+}
